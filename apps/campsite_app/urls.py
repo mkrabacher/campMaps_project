ec6be3a34b30ce0a3f74e048a6b8f1b145ebe2df
Ben Conaty <ben.conaty@gmail.com>
2018-01-30 15:51:52 -0800

    Changed the root route to campsite_app and made a index page

--- apps/campsite_app/urls.py
+++ apps/campsite_app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^user/$', views.user),
     url(r'^user/(?P<id>\d+)', views.user_id),
     url(r'^site/$', views.site),

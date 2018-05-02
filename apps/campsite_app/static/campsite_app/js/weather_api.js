e02f73c6ed359058e75f38afcc9613b4d3ea3d9e
Matt Krabacher <matt.krabacher@gmail.com>
2018-05-02 12:40:09 -0700

    fixed CORS issue with the dark sky weather api

--- apps/campsite_app/static/campsite_app/js/weather_api.js
+++ apps/campsite_app/static/campsite_app/js/weather_api.js
@@ -1,27 +1,31 @@
-
-var target_string = 'https://api.darksky.net/forecast/6f2ceccdc4c5425df7336b58b16618bd/' + site_lat + ',' + site_lon
-$.ajax({
-    url: target_string,
-    success: function(serverResponse) {
-        weatherGather(serverResponse)
+$(document).ready(function () {
+    var target_string = 'https://api.darksky.net/forecast/6f2ceccdc4c5425df7336b58b16618bd/' + site_lat + ',' + site_lon
+    $.get({
+        url: '/site/get_weather/' + site_lat + ',' + site_lon,
+        success: function(res) {
+            displayWeather(res)
+        }
+    })
+    
+    function displayWeather(serverData) {
+        console.log('res', serverData);
+        var daily_forecast = serverData['daily']
+        var day_int = new Date().getDay()
+        var week_arr = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat']
+        for (var i=0; i < 5; i++) {
+            if(daily_forecast['data'][i]['icon'] == 'partly-cloudy-night' || daily_forecast['data'][i]['icon'] == 'partly-cloudy-day') {
+                var desc = 'cloudy'
+            } else if(daily_forecast['data'][i]['icon'] == 'clear-day') {
+                var desc = 'sunny'
+            } else {
+                var desc = daily_forecast['data'][i]['icon']
+            }
+            icon = daily_forecast['data'][i]['icon']
+            $('#weatherdiv').append('<div><h4>' + week_arr[day_int] + '</h4><img src="/static/campsite_app/images/' + daily_forecast['data'][i]['icon'] + '.png" alt="' + daily_forecast['data'][i]['icon'] + '"><p>' + desc + '</p></div>')
+            day_int++
+            if (day_int > 6) {
+                day_int = 0;
+            }
+        }
     }
 })
-
-function weatherGather(serverData) {
-    var daily_forecast = serverData['daily']
-    var day_int = new Date().getDay()
-    var week_arr = ['Sun','Mon','Tue','Wed','Thur','Fri','Sat']
-    for (var i=0; i < 5; i++) {
-        if(daily_forecast['data'][i]['icon'] == 'partly-cloudy-night' || daily_forecast['data'][i]['icon'] == 'partly-cloudy-day') {
-            var desc = 'cloudy'
-        } else {
-            desc = daily_forecast['data'][i]['icon']
-        }
-        icon = daily_forecast['data'][i]['icon']
-        $('#weatherdiv').append('<div><h4>' + week_arr[day_int] + '</h4><img src="/static/campsite_app/images/' + daily_forecast['data'][i]['icon'] + '.png" alt="' + daily_forecast['data'][i]['icon'] + '"><p>' + desc + '</p></div>')
-        day_int++
-        if (day_int > 6) {
-            day_int = 0;
-        }
-    }
-}
--- apps/campsite_app/urls.py
+++ apps/campsite_app/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     url(r'^about/$', views.about),
     url(r'^site/(?P<id>\d+)', views.site_id),
     url(r'^site/add', views.site_add),
+    url(r'^site/get_weather/(?P<latlng>-?\d+\.\d+,-?\d+\.\d+)', views.get_weather),
     url(r'^process_add', views.process_add),
     url(r'^sites.json$', views.sites_json),
     url(r'^json_test$', views.json_test)
